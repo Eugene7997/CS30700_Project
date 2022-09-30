da8215590681e9a51b726b2ec09677f363dff52a
Ji Woong Park <park835@purdue.edu>
2022-09-30 01:07:23 -0400

    Add of header on website
    Added logo on the head of website and created header with text "Argus" and "About"

--- frontend/argus/src/App.js
+++ frontend/argus/src/App.js
@@ -1,38 +1,17 @@
 import React, {Component, ScrollAnimation, useRef} from 'react';
 
 
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router} from 'react-router-dom'
 
 
 
 function App() { 
-  const example1 = useRef(null);
-  const example2 = useRef(null);
-  const example3 = useRef(null);
-
-  const scrollTosection = (elementRef) => {
-    window.scrollTo({
-      top: elementRef.current.offsetTop,
-      behavior: 'smooth',
-    })
-  }
-
     return (
-      <div className='App'>
-        <div className='argus'>
-          <ul>
-            <li onClick={() => scrollTosection(example1)} className="link">Argus</li>
-          </ul>
-        </div>
-        <div className='example1'>
-          <li>Example1</li>
-        </div><div className='example2'>
-          <li>Example2</li>
-        </div><div className='example3'>
-          <li>Example3</li>
-        </div>
-      </div>
+      <Router>
+        <Navbar />
+      </Router>
     )
   
 }
