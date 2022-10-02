f60116acd05f45338c14859a52281f767e5312ad
Adam Rutledge <acrutled@purdue.edu>
2022-10-02 18:50:29 -0400

    created models and linked MySQL server

--- backend/arg/models.py
+++ backend/arg/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+# MySQL password: 307team19argus
 # Create your models here.
+
+
+class Region(models.Model):
+    region_id = models.IntegerField(primary_key=True)
+    region_name = models.CharField(max_length=30)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+
+class Environmental_Activity(models.Model):
+    ea_id = models.IntegerField(primary_key=True)
+    ea_name = models.CharField(max_length=50)
+
+class Datapoint(models.Model):
+    dp_id = models.IntegerField(primary_key=True)
+    region_id = models.ForeignKey(Region, on_delete=models.CASCADE)
+    ea_id = models.ForeignKey(Environmental_Activity, on_delete=models.CASCADE)
+    dp_datetime = models.DateTimeField()
+    is_future = models.BooleanField()
+    value = models.FloatField()
