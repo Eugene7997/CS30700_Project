b891d9dd66e50d2e8ae12763309fa000685e8bb4
Adam Rutledge <acrutled@purdue.edu>
2022-10-02 20:25:27 -0400

    set up django rest framework REST API, refer to MySQLREADME.txt in /docs/ directory to set up

--- backend/arg/urls.py
+++ backend/arg/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from . import views 
+from rest_framework import routers
+from arg.views import RegionViewSet, EnvironmentalActivityViewSet, DatapointViewSet
+
+router = routers.DefaultRouter()
+router.register(r'region', RegionViewSet)
+router.register(r'environmentalactivity', EnvironmentalActivityViewSet)
+router.register(r'datapoint', DatapointViewSet)
 
 #URLConf
 urlpatterns = [
-    path('hello/', views.say_hello)
+     path('hello/', views.say_hello),
+     path('', include(router.urls))
 ]
--- backend/arg/views.py
+++ backend/arg/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+
+from arg.serializers import RegionSerializer, DatapointSerializer, EnvironmentalActivitySerializer
+from arg.models import Region, Datapoint, EnvironmentalActivity
 
 # Create your views here.
 # request -> response
@@ -10,3 +14,15 @@ def say_hello(request):
     #x = 1
     #y = 2
     return render(request, 'hello.html', {'name': 'Mosh'})
+
+class RegionViewSet(viewsets.ModelViewSet):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+
+class DatapointViewSet(viewsets.ModelViewSet):
+    queryset = Datapoint.objects.all()
+    serializer_class = DatapointSerializer
+
+class EnvironmentalActivityViewSet(viewsets.ModelViewSet):
+    queryset = EnvironmentalActivity.objects.all()
+    serializer_class = EnvironmentalActivitySerializer
