22fe538b03f3140a8fc3f510c66b5e22b5edab50
Eugene <ypoh007@e.ntu.edu.sg>
2022-10-04 21:38:13 -0400

    Prototyping header of application page

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Head from '../header'
 
 const Application = () => {
   return (
-    <div>Application</div>
+    <div>
+      <div style = {{
+          backgroundColor: 'black',
+          opacity: '0.8'
+      }}>
+        <Head />
+      </div>
+      
+      <div>
+        <h1>Application</h1>
+      </div>
+    </div>
   )
 }
 
