57dfa5c77a753d9bc6c8cb641d643a0a91e39ab8
Eugene <ypoh007@e.ntu.edu.sg>
2022-10-05 00:29:29 -0400

    enable zoom in and out by mouse wheel

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -22,7 +22,7 @@ const Application = () => {
       </div>
       
       <div id="map">    
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
