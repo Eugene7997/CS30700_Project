7231ee2a16b3bc7e73e3342227d3fbb1186ee07d
Ji Woong Park <park835@purdue.edu>
2022-10-05 15:50:05 -0400

    Update index.js

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -1,20 +1,94 @@
 // react-learn JS libraries
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import '../../App.css';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from '../header'
-
+import L from "leaflet";
 import img from "./bg.jpg"
+import { LatLng } from "leaflet"
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+
+
+//function to search location by name
+const Search = (props) => {
+    const map = useMap()
+    const { provider } = props
+    // const [position, setPosition] = useState(null);
+    // const [bbox, setBbox] = useState([]);
+    // const result = provider.search({query: provider});
+    // provider.search({ query: '...' }).then(function (result) {
+    //   alert(result)
+    // });
+    // function searchEventHandler(result) { alert('hi')}
+    // const provider = new OpenStreetMapProvider();
+    // const results = provider.search({ query: input.value });
+    useEffect(() => {
+      
+        const searchControl = new GeoSearchControl({
+            provider,
+            autoComplete: true,
+            // style: 'button',
+            showPopup: false,
+            // resultFormat: ({result}) => result,
+            popupFormat: ({query, result}) => "x: "+result.x + " y: " + result.y,
+            // marker: {
+            //   // optional: L.Marker    - default L.Icon.Default
+            //   icon: new L.Icon.Default(),
+            //   draggable: false,
+            // },
+        }).addTo(map)
+        
+        return () => map.removeControl(searchControl)
+    }, [props])
+
+    return null // don't want anything to show up from this comp
+}
+
+//function to retrieve user's current location
+const CurrentLocation = () => {
+    const [position, setPosition] = useState(null);
+    const [bbox, setBbox] = useState([]);
+
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
+      });
+    }, [map]);
+
+    return position === null ? null : (
+        <Marker position={position}>
+          <Popup>
+            You are here. <br />
+            <b>SW lng</b>: {bbox[0]} <br />
+            <b>SW lat</b>: {bbox[1]} <br />
+            <b>NE lng</b>: {bbox[2]} <br />
+            <b>NE lat</b>: {bbox[3]}
+            
+          </Popup>
+        </Marker>
+      );
+}
+
 
 const Application = () => {
+
   return (
     <div style = {{
       backgroundImage: `url(${img})`,
-      backgroundSize: 'cover',
+      // backgroundSize: 'cover',
       backgroundRepeat: `no-repeat`,
       height: '100vh',
       backgroundPosition: 'center',
+      // alignContent: 'center',
+      // justifyContent: 'center'
     }}>
 
       <div>
@@ -27,11 +101,14 @@ const Application = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {/* <Marker position={[51.505, -0.09]}> */}
+            {/* <Popup>
+              You are here <br />
+              Temp: 0
+            </Popup> */}
+            <CurrentLocation />
+            <Search provider={new OpenStreetMapProvider()} />
+          {/* </Marker> */}
         </MapContainer>
       </div>
 
