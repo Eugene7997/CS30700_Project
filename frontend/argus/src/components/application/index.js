a5a21b76f92c3e84a65e30f80c3b6b798978fffb
Eugene <ypoh007@e.ntu.edu.sg>
2022-10-05 00:30:01 -0400

    changed map tile layer

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -25,7 +25,7 @@ const Application = () => {
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
           />
           <Marker position={[51.505, -0.09]}>
             <Popup>
