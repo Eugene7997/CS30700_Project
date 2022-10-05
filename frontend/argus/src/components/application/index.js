7ff2cac9fc45dda1be85d0a7b01817bbe437b73d
Eugene <ypoh007@e.ntu.edu.sg>
2022-10-04 23:21:13 -0400

    Prototyped sample leaflet map

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -1,3 +1,7 @@
+// react-learn JS libraries
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import '../../App.css';
+
 import React from 'react'
 import Head from '../header'
 
@@ -11,8 +15,18 @@ const Application = () => {
         <Head />
       </div>
       
-      <div>
-        <h1>Application</h1>
+      <div id="map">    
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   )
