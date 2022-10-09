f303e13d58ab92e4995e48fa331978103822eedf
Ji Woong Park <park835@purdue.edu>
2022-10-09 13:53:09 -0700

    Fetching weather data on the searched location
    Updated API to openweatherAPI and succeed to retrieve the weather data from API. Not yet showing the fetched data.

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -1,33 +1,150 @@
+
 // react-learn JS libraries
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import '../../App.css';
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Component } from 'react'
 import Head from '../header'
 import L from "leaflet";
 import img from "./bg.jpg"
 import { LatLng } from "leaflet"
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-import useFetch from "react-fetch-hook"
-import axios from 'axios'
 
 
 
+// class Application extends Component {
+//   state = {
+//     lat: undefined,
+//     lon: undefined,  
+//     city: undefined,
+//     temperatureC: undefined,
+//     temperatureF: undefined,
+//     icon: undefined,
+//     sunrise: undefined,
+//     sunset: undefined,
+//     errorMessage: undefined,
+//     APIKEY: "37cde85ed34605798aa360d4c26dc586"
+//   }
+//   getPosition = () => {
+//     return new Promise(function (resolve, reject) {
+//       navigator.geolocation.getCurrentPosition(resolve, reject);
+//     });    
+//   }
+
+//   getWeather = async (latitude, longitude) => {
+//     const apicall = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=37cde85ed34605798aa360d4c26dc586&units=metric`)
+//     const data = await apicall.json();
+//     this.setState({
+//       lat: latitude,
+//       lon: longitude,
+//       city: data.name,
+//       // temperatureC: Math.round(data.main.temp),
+//       // temperatureF: Math.round(data.main.temp * 1.8 + 32),
+//       // icon: data.weather[0].icon,
+//     })
+//   }
+
+//   componentDidMount() {
+//     this.getPosition()
+//     .then((position) => {      
+//       this.getWeather(position.coords.latitude, position.coords.longitude)
+//       alert(position.coords.latitude);
+//     })
+    
+//     .catch((err) => {
+//       this.setState({ errorMessage: err.message });
+//     });
+
+//     this.timerID = setInterval(        
+//       () => 
+//       this.getWeather(this.state.lat, this.state.lon),
+//       60000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   render() {
+//     return (
+//       <div style = {{
+//         backgroundImage: `url(${img})`,
+//         // backgroundSize: 'cover',
+//         backgroundRepeat: `no-repeat`,
+//         height: '100vh',
+//         backgroundPosition: 'center',
+//         // alignContent: 'center',
+//         // justifyContent: 'center'
+//       }}>
+  
+//         <div>
+//           <Head />
+//         </div>
+        
+//         <div id="map">    
+//           <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+//             <TileLayer
+//               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//             />
+//             {/* <Marker position={[51.505, -0.09]}> */}
+//               {/* <Popup>
+//                 You are here <br />
+//                 Temp: 0
+//               </Popup> */}
+//               <Search provider={new OpenStreetMapProvider()} /> */}
+//               <CurrentLocation />
+//               {/* <Fetchdata /> */}
+//             {/* </Marker> */}
+//           </MapContainer>
+//         </div>
+  
+//       </div>
+//     )
+//   }
+// }
+
+// export default Application;
 
 //function to search location by name
 const Search = (props)  => {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
-    const {isLoading, error, data } = useFetch(`https://api.open-meteo.com/v1/forecast?latitude=${y}&longitude=${x}&current_weather=true`)
+    // const [dt, setData] = useState();
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const map = useMap()
     const { provider } = props
     
+    
+
+    useEffect(() => {
+      Fetchdata();
+    }, [x,y])
+
+    const Fetchdata = async() => {
+      const APIKEY = "37cde85ed34605798aa360d4c26dc586"
+
+      const apicall = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${y}&lon=${x}&appid=${APIKEY}&units=metric`)
+      const dd = await apicall.json();
+      console.log(dd);
+      
+      return (<Marker>
+      <Popup>
+        You are here. <br />
+      </Popup>
+    </Marker>)
+      
+    }
 
     //retrieve the temperature data from API
-    const Temp = (x,y) => {
-      setX(x);
-      setY(y);
-      return (JSON.parse(JSON.stringify(data)).current_weather.temperature);
+    const Temp = (result) => {
+      setX(result.x);
+      setY(result.y);
+      // handleShow();
+      // return result.label
     }
 
     //search the location by location_label
@@ -35,8 +152,9 @@ const Search = (props)  => {
         const searchControl = new GeoSearchControl({
             provider,
             autoComplete: true,
-            showPopup: true,
-            popupFormat: ({query, result}) => "Temperature: " + Temp(result.x, result.y).toString()
+            showPopup: false,
+            showMarker: false,
+            popupFormat: ({query, result}) => Temp(result)
             // marker: {
             //   // optional: L.Marker    - default L.Icon.Default
             //   icon: new L.Icon.Default(),
@@ -46,6 +164,12 @@ const Search = (props)  => {
         
         return () => map.removeControl(searchControl)
     }, [props])
+
+    // useEffect(() => {
+    //   Fetchdata();
+    //   // const {isLoading, error, data } = useFetch(`https://api.open-meteo.com/v1/forecast?latitude=${y}&longitude=${x}&current_weather=true`)
+    //   // Fetchdata()
+    // }, [])
 
     return null // don't want anything to show up from this comp
 }
@@ -60,6 +184,7 @@ const CurrentLocation = () => {
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
         setPosition(e.latlng);
+        // alert(e.latlng);
         map.flyTo(e.latlng, map.getZoom());
         const radius = e.accuracy;
         const circle = L.circle(e.latlng, radius);
@@ -113,7 +238,7 @@ const Application = () => {
             </Popup> */}
             <Search provider={new OpenStreetMapProvider()} />
             <CurrentLocation />
-            
+            {/* <Fetchdata /> */}
           {/* </Marker> */}
         </MapContainer>
       </div>
@@ -122,4 +247,4 @@ const Application = () => {
   )
 }
 
-export default Application
+export default Application;
