fac10199511a8cd459605ac47214d31b40820650
Bhavik Sardar <bhaviksardar@Bhaviks-MacBook-Pro.local>
2022-10-09 18:47:49 -0400

    Added precipitation data preprocess

--- backend/arg/preprocess.py
+++ backend/arg/preprocess.py
@@ -9,8 +9,6 @@ import numpy as np
 #%%
 #data = pd.read_csv("tempcsvfile")
 
-
-
 date = data['DATE'].tolist()
 
 dates = {}
@@ -26,3 +24,26 @@ for i in range(len(max)):
             nmax = (dates[date[i]][0] + max[i]) / 2
             nmin = (dates[date[i]][1] + min[i]) / 2
             dates[date[i]] = [nmax, nmin]
+
+# Preprocessing Precipitation CSV Data
+#%%
+
+precip_dates = {}
+
+precip = data['PRCP'].tolist()
+
+precip_new = []
+
+for i in precip:
+    if (isnan(i)):
+        precip_new.append(0.0)
+    else:
+        precip_new.append(i)
+
+for i in range(len(date)):
+    if (isnan(precip_new[i]) == False):
+        if date[i] not in precip_dates.keys():
+            precip_dates[date[i]] = [precip_new[i]]
+        else:
+            nprecip = (precip_dates[date[i]][0] + precip_new[i]) / 2
+            precip_dates[date[i]] = [nprecip]
