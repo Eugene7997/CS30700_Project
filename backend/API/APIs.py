ab2180918b81a05d07c5c14e659f7de8a5719107
Ji Woong Park <park835@purdue.edu>
2022-10-23 16:56:40 -0400

    GHG API added
    Created a function to fetch the GHG data from API

--- backend/API/APIs.py
+++ backend/API/APIs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[2]:
 
 
 import requests
@@ -73,13 +73,13 @@ json_data = response.json()
 json_data
 
 
-# In[42]:
+# In[52]:
 
 
 response = requests.get('https://www.worldtides.info/api/v3?heights&date=2022-10-23&lat=44.34&lon=10.99&key=2ff4df9a-2263-4c2c-a8b8-e8c11d46331f')
 
 
-# In[51]:
+# In[53]:
 
 
 json_data = response.json()
@@ -94,28 +94,74 @@ json_data['requestLat']
 
 # Greenhouse gas emissions
 
-# In[ ]:
+# In[64]:
 
 
+response = requests.get('https://api.v2.emissions-api.org'
+        + '/api/v2/carbonmonoxide/average.json'
+        + '?country=DE&begin=2022-10-20&end=2022-10-22')
 
 
-
-# In[ ]:
-
+# In[65]:
 
 
+response.json()
 
 
-# In[ ]:
+# In[11]:
 
 
+response = requests.get('https://api.co2signal.com/v1/latest?lon=10.99&lat=44.34',
+                       headers={
+                           'auth-token': 'S3Hlk9xkYNaGmqYn8G1JoIH0QPiJsn55'
+                       })
 
 
-
-# In[ ]:
-
+# In[12]:
 
 
+response.json()
+
+
+# In[13]:
+
+
+response = requests.get('https://api.ambeedata.com/latest/by-lat-lng?lat=12&lng=77',
+                       headers={
+                           "x-api-key": "b1637cd664e7dce01cbd651b44e311e27b269c4717eb903fe290388116354b69",
+                       })
+
+
+# In[14]:
+
+
+response.json()
+
+
+# In[15]:
+
+
+API = '37cde85ed34605798aa360d4c26dc586'
+response = requests.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=44.34&lon=10.99&appid=37cde85ed34605798aa360d4c26dc586')
+
+
+# In[22]:
+
+
+res = response.text
+parse_json = json.loads(res)
+
+
+# In[44]:
+
+
+parse_json['list'][0]['components']['o3'] # ozone
+
+
+# In[45]:
+
+
+parse_json['list'][0]['components']['no2']
 
 
 # In[ ]:
