bb1ebfd3638db5029fb355c6ddfba75cbd8de260
Eugene <ypoh007@e.ntu.edu.sg>
2022-11-04 19:34:46 -0400

    fixed indentation

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -16,129 +16,114 @@ import Chloropleth_legends from '../chloropleth_map/chloropleth_legends';
 
 //function to search location by name
 const Search = (props)  => {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
-    const [lab, setLabel] = useState(null);
-    const map = useMap()
-    const { provider } = props
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [lab, setLabel] = useState(null);
+  const map = useMap()
+  const { provider } = props
 
-    useEffect(() => {
-      //Fetchdata();
-    }, [x,y])
+  useEffect(() => {
+    //Fetchdata();
+  }, [x,y])
 
+  //retrieve the temperature and weather data when user searched location
+  const Fetchdata = async() => {
+    const APIKEY = "37cde85ed34605798aa360d4c26dc586"
+    const apicall = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${y}&lon=${x}&appid=${APIKEY}&units=metric`)
+    const dd = await apicall.json();
+    console.log(
+      "Label: " + lab + "\n" +
+      "Temp: " + dd.main.temp + "\n" +
+      "Temp (feels like): " + dd.main.feels_like + "\n" +
+      "Temp (min): " + dd.main.temp_min + "\n" +
+      "Temp (max): " + dd.main.temp_max + "\n" +
+      "Pressure: " + dd.main.pressure +"\n" +
+      "Temp: " + dd.main.temp +"\n" +
+      "Temp: " + dd.main.temp + "\n" +
+      "Weather: " + dd.weather[0].main+ "\n" +
+      "Detailed weather: " + dd.weather[0].description
+    )
+  }
 
-    //retrieve the temperature and weather data when user searched location
-    const Fetchdata = async() => {
-     const APIKEY = "37cde85ed34605798aa360d4c26dc586"
-     const apicall = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${y}&lon=${x}&appid=${APIKEY}&units=metric`)
-      const dd = await apicall.json();
-     console.log(
-       "Label: " + lab + "\n"
-     + "Temp: " + dd.main.temp + "\n"
-     + "Temp (feels like): " + dd.main.feels_like + "\n"
-     + "Temp (min): " + dd.main.temp_min + "\n"
-     + "Temp (max): " + dd.main.temp_max + "\n"
-     + "Pressure: " + dd.main.pressure +"\n"
-     + "Temp: " + dd.main.temp +"\n"
-     + "Temp: " + dd.main.temp + "\n"
-     + "Weather: " + dd.weather[0].main+ "\n"
-     + "Detailed weather: " + dd.weather[0].description);
-      
-      
-   }
-
-   let data = {
+  let data = {
     'latitude': y,
     'longitude': x
-   }
+  }
 
   //creating react post request and fetching data from django
+  const response = fetch('http://127.0.0.1:8000/arg/api/', {
+    method: 'POST',
+    body : JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json, text/plain',
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  }).then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+    .catch(error => console.log("Error detected: " + error))
+  
+  //search the location by location_label
+  useEffect(()  => {
+    const searchControl = new GeoSearchControl({
+      provider,
+      autoComplete: true,
+      showPopup: false,
+      showMarker: true,
+      popupFormat: ({query, result}) => {
+        setX(result.x); 
+        setY(result.y);
+        setLabel(result.label)
+        return result.label
+      }
+    }).addTo(map)
+    return () => map.removeControl(searchControl)
+  }, [props])
+
+  return  null // don't want anything to show up from this comp
+}
+
 const response = fetch('http://127.0.0.1:8000/arg/api/', {
   method: 'POST',
-  body : JSON.stringify(data),
+  body: JSON.stringify({
+    LatLng
+  }),
   headers: {
     'Accept': 'application/json, text/plain',
     'Content-Type': 'application/json; charset=utf-8'
   }
-
-
 }).then(response => response.json())
-.then(data => console.log(JSON.stringify(data)))
-.catch(error => console.log("Error detected: " + error));
-
-      
-    //search the location by location_label
-    useEffect(()  => {
-        const searchControl = new GeoSearchControl({
-            provider,
-            autoComplete: true,
-            showPopup: false,
-            showMarker: true,
-            popupFormat: ({query, result}) => {
-              setX(result.x); 
-              setY(result.y);
-              setLabel(result.label);
-              return result.label;
-            }
-            
-        }).addTo(map)
-        return () => map.removeControl(searchControl)
-    }, [props])
-
-
-    return  null // don't want anything to show up from this comp
-}
-
- const response = fetch('http://127.0.0.1:8000/arg/api/', {
-        method: 'POST',
-        body: JSON.stringify({
-            LatLng
-        }),
-        headers: {
-          'Accept': 'application/json, text/plain',
-          'Content-Type': 'application/json; charset=utf-8'
-        }
-
- }).then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log("Error detected: " + error));
-  
-   
-
-
-      
-      
+  .then(data => console.log(data))
+  .catch(error => console.log("Error detected: " + error))
 
 //function to retrieve user's current location
 const CurrentLocation = () => {
-    const [position, setPosition] = useState(null);
-    const [bbox, setBbox] = useState([]);
+  const [position, setPosition] = useState(null)
+  const [bbox, setBbox] = useState([])
+  const map = useMap()
 
-    const map = useMap();
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng)
+      // alert(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      const radius = e.accuracy
+      const circle = L.circle(e.latlng, radius)
+      circle.addTo(map)
+      setBbox(e.bounds.toBBoxString().split(","))
+    });
+  }, [map])
 
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        // alert(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const radius = e.accuracy;
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
-      });
-    }, [map]);
-
-    return position === null ? null : (
-        <Marker position={position}>
-          <Popup>
-            You are here. <br />
-            <b>SW lng</b>: {bbox[0]} <br />
-            <b>SW lat</b>: {bbox[1]} <br />
-            <b>NE lng</b>: {bbox[2]} <br />
-            <b>NE lat</b>: {bbox[3]}
-          </Popup>
-        </Marker>
-      );
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        You are here. <br />
+        <b>SW lng</b>: {bbox[0]} <br />
+        <b>SW lat</b>: {bbox[1]} <br />
+        <b>NE lng</b>: {bbox[2]} <br />
+        <b>NE lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
+  )
 }
 
 const {BaseLayer} = LayersControl
@@ -153,6 +138,7 @@ const Application = () => {
       fillOpacity: 1
     })
   })
+
   const resetHighlight= (e =>{
     e.target.setStyle(chloropleth_style(e.target.feature));
   })
@@ -163,35 +149,35 @@ const Application = () => {
     const temp_celsius = feature.properties.tempchg_c
     layer.bindPopup(`<strong>name: ${name} <br/> temp change in celsius: ${temp_celsius} </strong>`)
     layer.on({
-        mouseover: highlightChloropleth,
-        mouseout: resetHighlight,
-    });
+      mouseover: highlightChloropleth,
+      mouseout: resetHighlight,
+    })
   }
 
   const mapPolygonColorToDensity=(value => {
     return value > 1
-        ? '#a50f15'
-        : value > 0.75
-        ? '#de2d26'
-        : value > 0.50
-        ? '#fb6a4a'
-        : value > 0.25
-        ? '#fc9272'
-        : value > 0
-        ? '#fcbba1'
-        : '#fee5d9';
+      ? '#a50f15'
+      : value > 0.75
+      ? '#de2d26'
+      : value > 0.50
+      ? '#fb6a4a'
+      : value > 0.25
+      ? '#fc9272'
+      : value > 0
+      ? '#fcbba1'
+      : '#fee5d9';
   })
 
   const chloropleth_style = (feature => {
     return ({
-        fillColor: mapPolygonColorToDensity(feature.properties.tempchg),
-        weight: 1,
-        opacity: 1,
-        color: 'white',
-        dashArray: '2',
-        fillOpacity: 0.5
-    });
-  }); 
+      fillColor: mapPolygonColorToDensity(feature.properties.tempchg),
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      dashArray: '2',
+      fillOpacity: 0.5
+    })
+  })
 
   const mapStyle = {
     margin: '0 auto',
