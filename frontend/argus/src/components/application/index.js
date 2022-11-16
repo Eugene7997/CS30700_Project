80005e002511ccc37f3b5417de67f3e05474b70d
Eugene <ypoh007@e.ntu.edu.sg>
2022-11-16 17:30:38 -0500

    implemented choropleth map for all EAs

--- frontend/argus/src/components/application/index.js
+++ frontend/argus/src/components/application/index.js
@@ -173,96 +173,150 @@ const CurrentLocation = () => {
   )
 }
 
-const ChoroplethMap = (props) => {
+const Choropleth = () => {
 
-  const [legendToggle, setLegendToggle] = useState(false)
-  const [geoData, setGeoData] = useState(null)
+  const ChoroplethMap = (props) => {
 
-  useEffect(() => {
-    fetchGeoData()
-  },[legendToggle,geoData])
-  
-  const fetchGeoData = async() => {
-    const response = await fetch('http://127.0.0.1:8000/arg/geojson/', {
-      method: 'POST',
-      body : JSON.stringify({'ea': props.ea_type, 'datetime': new Date().toISOString().split('.')[0]}),
-      headers: {
-        'Accept': 'application/json, text/plain',
-        'Content-Type': 'application/json; charset=utf-8'
+    const [legendToggle, setLegendToggle] = useState(false)
+    const [geoData, setGeoData] = useState(null)
+
+    useEffect(() => {
+      fetchGeoData()
+    })
+    
+    const fetchGeoData = async() => {
+      const response = await fetch('http://127.0.0.1:8000/arg/geojson/', {
+        method: 'POST',
+        body : JSON.stringify({'ea': props.ea_type, 'datetime': new Date().toISOString().split('.')[0]}),
+        headers: {
+          'Accept': 'application/json, text/plain',
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      })
+      const res = await response.json();
+      setGeoData(res)
+    }
+
+    const highlightChloropleth = (e => {
+      var layer = e.target
+      layer.setStyle({
+        weight: 1,
+        color: "black",
+        fillOpacity: 1
+      })
+    })
+
+    const resetHighlight= (e =>{
+      e.target.setStyle(chloropleth_style(e.target.feature));
+    })
+
+    const onEachFeature= (feature, layer)=> {
+      console.log("onEachFeature", feature)
+      const name = feature.properties.ADMIN
+      const value = feature.properties.value
+      layer.bindPopup(`<strong>name: ${name} <br/> ${props.ea_type}: ${value} </strong>`)
+      layer.on({
+        mouseover: highlightChloropleth,
+        mouseout: resetHighlight,
+      })
+    }
+
+    const mapPolygonColorToDensity=(value => {
+      if (props.ea_type === "temperature") {
+        return value > 10
+        ? '#a50f15'
+        : value > 5
+        ? '#de2d26'
+        : value > 4
+        ? '#fb6a4a'
+        : value > 3
+        ? '#fc9272'
+        : value > 2
+        ? '#fcbba1'
+        : '#fee5d9';
       }
+      else if (props.ea_type === "humidity") {
+        return value > 10
+        ? '#FF8300'
+        : value > 5
+        ? '#FE992D'
+        : value > 4
+        ? '#FFA84B'
+        : value > 3
+        ? '#FFBF7B'
+        : value > 2
+        ? '#FFD7AC'
+        : '#FCE0C2';
+      }
+      else if (props.ea_type === "sea level") {
+        return value > 10
+        ? '#005D59'
+        : value > 5
+        ? '#00746F'
+        : value > 4
+        ? '#0CD1CA'
+        : value > 3
+        ? '#1EE1DA'
+        : value > 2
+        ? '#94F3EF'
+        : '#CFFCFA';
+      }
+      else if (props.ea_type === "GHG") {
+        return value > 10
+        ? '#006834'
+        : value > 5
+        ? '#009149'
+        : value > 4
+        ? '#00BE60'
+        : value > 3
+        ? '#00DA6F'
+        : value > 2
+        ? '#A4ECC8'
+        : '#C9EEDC';
+      }
+      else {
+        return null
+      }
+      
     })
-    const res = await response.json();
-    setGeoData(res)
-  }
 
-  const highlightChloropleth = (e => {
-    var layer = e.target
-    layer.setStyle({
-      weight: 1,
-      color: "black",
-      fillOpacity: 1
+    const chloropleth_style = (feature => {
+      return ({
+        fillColor: mapPolygonColorToDensity(feature.properties.value),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '2',
+        fillOpacity: 0.5
+      })
     })
-  })
-
-  const resetHighlight= (e =>{
-    e.target.setStyle(chloropleth_style(e.target.feature));
-  })
-
-  const onEachFeature= (feature, layer)=> {
-    console.log("onEachFeature", feature)
-    const name = feature.properties.ADMIN
-    const value = feature.properties.value
-    layer.bindPopup(`<strong>name: ${name} <br/> temp: ${value} </strong>`)
-    layer.on({
-      mouseover: highlightChloropleth,
-      mouseout: resetHighlight,
-    })
-  }
-
-  const mapPolygonColorToDensity=(value => {
-    return value > 10
-      ? '#a50f15'
-      : value > 5
-      ? '#de2d26'
-      : value > 4
-      ? '#fb6a4a'
-      : value > 3
-      ? '#fc9272'
-      : value > 2
-      ? '#fcbba1'
-      : '#fee5d9';
-  })
-
-  const chloropleth_style = (feature => {
-    return ({
-      fillColor: mapPolygonColorToDensity(feature.properties.value),
-      weight: 1,
-      opacity: 1,
-      color: 'white',
-      dashArray: '2',
-      fillOpacity: 0.5
-    })
-  })
-  
-  return (
-    <LayersControl>
-      <LayersControl.Overlay name = "Choropleth map - Temperature">
-        <LayerGroup
-          eventHandlers = {
-            {
-              add:() => {
-                setLegendToggle(true)
-              },
-              remove:() => {
-                setLegendToggle(false)
+    
+    return (
+        <LayersControl.Overlay name = {`Choropleth map - ${props.ea_type}`}>
+          <LayerGroup
+            eventHandlers = {
+              {
+                add:() => {
+                  setLegendToggle(true)
+                },
+                remove:() => {
+                  setLegendToggle(false)
+                }
               }
             }
-          }
-        >
-          { geoData && (<GeoJSON data = {geoData} onEachFeature={onEachFeature} style = {chloropleth_style}/>) }
-        </LayerGroup>
-      </LayersControl.Overlay>
-      {legendToggle ? <Chloropleth_legends/> : null}
+          >
+            { geoData && (<GeoJSON data = {geoData} onEachFeature={onEachFeature} style = {chloropleth_style}/>) }      
+          </LayerGroup>
+          {legendToggle ? <Chloropleth_legends ea_type={props.ea_type}/> : null}
+        </LayersControl.Overlay>
+    )
+  }
+  return (
+    <LayersControl>
+      <ChoroplethMap ea_type="temperature"/>
+      <ChoroplethMap ea_type="humidity"/>
+      <ChoroplethMap ea_type="sea level"/>
+      <ChoroplethMap ea_type="GHG"/>
     </LayersControl>
   )
 }
@@ -370,7 +424,8 @@ const Application = () => {
               />
             </BaseLayer>
           </LayersControl>
-          <ChoroplethMap ea_type="temperature"/>a
+          <Choropleth/>
+          {/* <ChoroplethMap ea_type="temperature"/> */}
           <Earthquake/>
           <Search provider={new OpenStreetMapProvider()} />
           <CurrentLocation />
--- frontend/argus/src/components/chloropleth_map/chloropleth_legends.js
+++ frontend/argus/src/components/chloropleth_map/chloropleth_legends.js
@@ -1,18 +1,60 @@
 import React from 'react'
 import "./chloropleth_legends_style.css"
 
-const Chloropleth_legends = () => {
+const Chloropleth_legends = (props) => {
 
-    return (
-        <div className="chloropleth_legends">
-            <div style={{ "--color": '#a50f15' }}>Hot</div  >
-            <div style={{ "--color": '#de2d26' }}>mildly hot</div>
-            <div style={{ "--color": '#fb6a4a' }}>warm</div>
-            <div style={{ "--color": '#fc9272' }}>normal</div>
-            <div style={{ "--color": '#fcbba1'}}>cold</div>
-            <div style={{ "--color": '#fee5d9' }}>very cold</div>
-        </div>
-    );
+    if (props.ea_type === "temperature") {
+        return (
+            <div className="chloropleth_legends">
+                <div style={{ "--color": '#a50f15' }}>Hot</div  >
+                <div style={{ "--color": '#de2d26' }}>mildly hot</div>
+                <div style={{ "--color": '#fb6a4a' }}>warm</div>
+                <div style={{ "--color": '#fc9272' }}>normal</div>
+                <div style={{ "--color": '#fcbba1'}}>cold</div>
+                <div style={{ "--color": '#fee5d9' }}>very cold</div>
+            </div>
+        );
+    }
+    else if (props.ea_type === "sea level") {
+        return (
+            <div className="chloropleth_legends">
+                <div style={{ "--color": '#005D59' }}>High</div  >
+                <div style={{ "--color": '#00746F' }}>mildly high</div>
+                <div style={{ "--color": '#0CD1CA' }}>slightly high</div>
+                <div style={{ "--color": '#1EE1DA' }}>normal</div>
+                <div style={{ "--color": '#94F3EF'}}>low</div>
+                <div style={{ "--color": '#CFFCFA' }}>very low</div>
+            </div>
+        );
+    }
+    else if (props.ea_type === "GHG") {
+        return (
+            <div className="chloropleth_legends">
+                <div style={{ "--color": '#006834' }}>Dense</div  >
+                <div style={{ "--color": '#009149' }}>mildly dense</div>
+                <div style={{ "--color": '#00BE60' }}>slightly dense</div>
+                <div style={{ "--color": '#00DA6F' }}>normal</div>
+                <div style={{ "--color": '#A4ECC8'}}>sparse</div>
+                <div style={{ "--color": '#C9EEDC' }}>very sparse</div>
+            </div>
+        )
+    }
+    else if (props.ea_type === "humidity") {
+        return (
+            <div className="chloropleth_legends">
+                <div style={{ "--color": '#FF8300' }}>Highly Humid</div  >
+                <div style={{ "--color": '#FE992D' }}>mildly Humid</div>
+                <div style={{ "--color": '#FFA84B' }}>slightly Humid</div>
+                <div style={{ "--color": '#FFBF7B' }}>normal</div>
+                <div style={{ "--color": '#FFD7AC'}}>dry</div>
+                <div style={{ "--color": '#FCE0C2' }}>very dry</div>
+            </div>
+        )
+    }
+    else {
+        return null
+    }
+
 }
 
 export default Chloropleth_legends
